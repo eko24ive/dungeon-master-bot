5229213f6404c5dddf60678c3d2744e83ae7b0c0
eko24ive <eko24old@gmail.com>
2018-11-18 13:29:15 +0200

    moved out dungeons to constants

---
+++ src/constants/dungeonNameMapping.js
@@ -0,0 +1,50 @@
+module.exports = {
+  oldMine: {
+    distance: '11км',
+    name: '⛰ Старая шахта',
+  },
+  haloCave: {
+    distance: '19км',
+    name: '⚠️ Пещера Ореола',
+  },
+  sewerPipe: {
+    distance: '23км',
+    name: '🚽 Сточная труба',
+  },
+  openVault: {
+    distance: '29км',
+    name: '⚙️Открытое убежище',
+  },
+  betCave: {
+    distance: '34км',
+    name: '🦇Бэт-пещера',
+  },
+  utkinPass: {
+    distance: '39км',
+    name: '🦆Перевал Уткина',
+  },
+  hroshgarHigh: {
+    distance: '45км',
+    name: '🌁Высокий Хротгар',
+  },
+  ruinsOfHexagon: {
+    distance: '50км',
+    name: '🛑 Руины Гексагона',
+  },
+  scientificComplex: {
+    distance: '56км',
+    name: '🔬Научный комплекс',
+  },
+  templeOfKnowledge: {
+    distance: '69км',
+    name: '⛩Храм Знаний',
+  },
+  blackMesa: {
+    distance: '74км',
+    name: '🗨Черная Меза',
+  },
+  moltenСore: {
+    distance: '80км',
+    name: '🔥Огненные недра',
+  },
+};
